d183fb8d66772a76289f09c87543692067c224fe
jaemin <als982001@naver.com>
2024-03-17 16:27:36 +0900

    주어진 데이터 이용

---
+++ src/components/ContainerComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Post v-for="(item, index) in data" :key="index" :item="item" />
+  </div>
+</template>
+
+<script>
+import PostComponentVue from "./PostComponent.vue";
+
+export default {
+  name: "ContainerComponent",
+  components: {
+    Post: PostComponentVue,
+  },
+  props: {
+    data: Array,
+  },
+};
+</script>
+
+<style></style>
