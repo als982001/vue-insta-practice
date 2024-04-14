a6db4e68e511dbb2dd3cef363d7926db68f2d0c3
jaemin <als982001@naver.com>
2024-04-14 14:08:39 +0900

    followers 보여주기

--- src/components/MyPageComponent.vue
+++ src/components/MyPageComponent.vue
@@ -6,8 +6,18 @@
       <div class="profile"></div>
       <span class="profile-name">사용자명</span>
     </div>
-    <div>
-      <div>{{ followers }}</div>
+    <div class="followers-wrapper">
+      <div
+        v-for="follower in followers"
+        :key="follower.name"
+        class="post-header"
+      >
+        <div
+          class="profile"
+          :style="{ backgroundImage: `url(${follower.image})` }"
+        ></div>
+        <div class="profile-name">{{ follower.name }}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -33,4 +43,15 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.followers-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  border-top: 1px solid black;
+  margin-top: 10px;
+}
+.follower-container {
+  background-color: gray;
+}
+</style>
